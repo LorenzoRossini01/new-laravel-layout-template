f3d751384e9920ee16ce08cf9749eca81e781d06
LorenzoRossini01 <lory.rox.01@gmail.com>
2024-03-20 09:59:22 +0100

    fix: header nav links

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -9,28 +9,9 @@
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
           <li class="nav-item">
-            <a aria-current="page" class="nav-link active" href="#">Home</a>
+            <a aria-current="page" @class(['nav-link', 'active' => Route::currentRouteName()=='home']) href="{{route('home')}}">Home</a>
           </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
-          </li>
-          <li class="nav-item dropdown">
-            <a aria-expanded="false" class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#"
-              role="button">
-              Dropdown
-            </a>
-            <ul class="dropdown-menu">
-              <li><a class="dropdown-item" href="#">Action</a></li>
-              <li><a class="dropdown-item" href="#">Another action</a></li>
-              <li>
-                <hr class="dropdown-divider">
-              </li>
-              <li><a class="dropdown-item" href="#">Something else here</a></li>
-            </ul>
-          </li>
-          <li class="nav-item">
-            <a aria-disabled="true" class="nav-link disabled">Disabled</a>
-          </li>
+
         </ul>
         <form class="d-flex" role="search">
           <input aria-label="Search" class="form-control me-2" placeholder="Search" type="search">
